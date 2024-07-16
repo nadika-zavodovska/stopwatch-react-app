53c87a6288dde1404f81ff1821d8b4f90d312cf5
Nadika <nadika.name@gmail.com>
2024-07-16 16:50:28 +0100

    start: add structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+import Stopwatch from './Stopwatch';
+
+function App() {
+
+  return (
+    <Stopwatch />
+  )
+}
+
+export default App;
---
+++ src/Stopwatch.jsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+function Stopwatch(){
+
+    const [isRunning, setIsRunning] = useState(false);
+
+    const [elapsedTime, setElapsedTime] = useState(0);
+
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+
+    },[isRunning]);
+
+    function start(){
+
+    }
+
+    function stop (){
+
+    }
+
+    function reset(){
+
+    }
+
+    function formatTime(){
+        return`00:00:00`;
+    }
+
+    return(
+        <div className="Stopwatch">
+            <div className="display">{formatTime()}</div>
+            <div className="controls">
+                <button className="start-button" onClick={start}>Start</button>
+                <button className="stop-button" onClick={stop}>Stop</button>
+                <button className="reset-button" onClick={reset}>Reset</button>
+            </div>
+        </div>
+    )
+}
+
+export default Stopwatch;
